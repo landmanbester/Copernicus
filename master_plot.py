5f4dd19bb15f0d7ec6a89d73f7c3d3a24ec22869
landmanbester <lbester@gmail.com>
2017-03-14 11:12:43 +0200

    Changed master_plot.py to adhere to new interface

--- master_plot.py
+++ master_plot.py
@@ -13,7 +13,7 @@ from matplotlib.patches import Rectangle
 from Copernicus.Parset import MyOptParse
 from Plotter import plh
 
-def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF):
+def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF,samps_out_name):
     print "Getting LCDM vals"
     # Get FLRW funcs for comparison
     Om0 = 0.3
@@ -38,15 +38,12 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF):
     # Do LCDM integration
     UF = SSU(zmax, tmin, Np, err, XH, Xrho, sigmaLam, Nret, data_prior, data_lik, fname, DoPLCF, Hz=HzF, rhoz=rhozF, Lam=LamF, useInputFuncs=True)
 
-    # Get quantities of interrest
-    if DoPLCF:
-        T1iF, T1fF, T2iF, T2fF, LLTBConsiF, LLTBConsfF, DiF, DfF, SiF, \
-        SfF, QiF, QfF, AiF, AfF, ZiF, ZfF, SpiF, SpfF, QpiF, QpfF, \
-        ZpiF, ZpfF, uiF, ufF, upiF, upfF, uppiF, uppfF, udotiF, udotfF, \
-        rhoiF, rhofF, rhopiF, rhopfF, rhodotiF, rhodotfF, DzF, dzdwzF, sigmasqiF, sigmasqfF = UF.get_funcs()
-    else:
-        T1iF, T2iF, LLTBConsiF, DiF, SiF, QiF, AiF, ZiF, SpiF, QpiF, ZpiF, uiF, upiF, uppiF, udotiF, rhoiF, rhopiF, \
-        rhodotiF, DzF, dzdwzF, sigmasqiF, = UF.get_funcs()
+    T1iF, T2iF, LLTBConsiF, DiF, SiF, QiF, AiF, ZiF, SpiF, QpiF, ZpiF, uiF, upiF, uppiF, udotiF, rhoiF, rhopiF, rhodotiF, \
+    DzF, dzdwzF, sigmasqiF, t0F = UF.get_funcsi()
+
+    if t0F > UF.tmin and UF.NI > 1 and DoPLCF:
+        T1fF, T2fF, LLTBConsfF, DfF, SfF, QfF, AfF, ZfF, SpfF, QpfF, ZpfF, ufF, upfF, uppfF, udotfF, rhofF, rhopfF, \
+        rhodotfF, sigmasqfF = UF.get_funcsf()
 
     sigmasqiF = sigmasqo(np.linspace(0, 1, Nret))
 
@@ -63,15 +60,12 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF):
     # Do LTBCon integration
     ULT = SSU(zmax, tmin, Np, err, XH, Xrho, sigmaLam, Nret, data_prior, data_lik, fname, DoPLCF, Hz=HzLT, rhoz=rhozLT, Lam=0.0, useInputFuncs=True)
 
-    # Get quantities of interrest
-    if DoPLCF:
-        T1iLT, T1fLT, T2iLT, T2fLT, LLTBConsiLT, LLTBConsfLT, DiLT, DfLT, SiLT, \
-        SfLT, QiLT, QfLT, AiLT, AfLT, ZiLT, ZfLT, SpiLT, SpfLT, QpiLT, QpfLT, \
-        ZpiLT, ZpfLT, uiLT, ufLT, upiLT, upfLT, uppiLT, uppfLT, udotiLT, udotfLT, \
-        rhoiLT, rhofLT, rhopiLT, rhopfLT, rhodotiLT, rhodotfLT, DzLT, dzdwzLT, sigmasqiLT, sigmasqfLT = ULT.get_funcs()
-    else:
-        T1iLT, T2iLT, LLTBConsiLT, DiLT, SiLT, QiLT, AiLT, ZiLT, SpiLT, QpiLT, ZpiLT, uiLT, upiLT, uppiLT, udotiLT, \
-        rhoiLT, rhopiLT, rhodotiLT, DzLT, dzdwzLT, sigmasqiLT, = ULT.get_funcs()
+    T1iLT, T2iLT, LLTBConsiLT, DiLT, SiLT, QiLT, AiLT, ZiLT, SpiLT, QpiLT, ZpiLT, uiLT, upiLT, uppiLT, udotiLT, rhoiLT, rhopiLT, rhodotiLT, \
+    DzLT, dzdwzLT, sigmasqiLT, t0LT = ULT.get_funcsi()
+
+    if t0LT > ULT.tmin and ULT.NI > 1 and DoPLCF:
+        T1fLT, T2fLT, LLTBConsfLT, DfLT, SfLT, QfLT, AfLT, ZfLT, SpfLT, QpfLT, ZpfLT, ufLT, upfLT, uppfLT, udotfLT, rhofLT, rhopfLT, \
+        rhodotfLT, sigmasqfLT = ULT.get_funcsf()
 
     # Load the data we want to plot
     files = ["DHt0/", "Ddzdw/", "DHdzdw/"]
@@ -81,7 +75,7 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF):
     dzdwdict = {}
     Lamdict = {}
     sigmasqidict = {}
-    #sigmasqfdict = {}
+    sigmasqfdict = {}
     Om0dict = {}
     OL0dict = {}
     colourdict = {}
@@ -95,14 +89,14 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF):
 
     # Load first samples
     for s in files:
-        with np.load("/home/landman/Projects/CP_LCDM_" + s + 'Processed_Data/samps0.npz') as holder:
+        with np.load("/home/landman/Projects/CP_LCDM_" + s + 'Processed_Data/' + samps_out_name + '.npz') as holder:
             Dzlist = holder['Dz']
             Hzlist = holder['Hz']
             rhozlist = holder['rhoz']
             dzdwzlist = holder['dzdwz']
             Lamlist = holder['Lam']
             sigmasqilist = holder['sigmasqi']
-            #sigmasqflist = holder['sigmasqf']
+            sigmasqflist = holder['sigmasqf']
             NSamplers = holder['NSamplers']
 
             # Load the rest of the data
@@ -114,7 +108,7 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF):
                     dzdwzsamps = np.append(dzdwzsamps, dzdwzlist[i], axis=1)
                     Lamsamps = np.append(Lamsamps, Lamlist[i])
                     sigmasqisamps = np.append(sigmasqisamps, sigmasqilist[i], axis=1)
-                    #sigmasqfsamps = np.append(sigmasqfsamps, sigmasqflist[i], axis=1)
+                    sigmasqfsamps = np.append(sigmasqfsamps, sigmasqflist[i], axis=1)
                 else:
                     Dzsamps = Dzlist[0]
                     Hzsamps = Hzlist[0]
@@ -122,7 +116,7 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF):
                     dzdwzsamps = dzdwzlist[0]
                     Lamsamps = Lamlist[0]
                     sigmasqisamps = sigmasqilist[0]
-                    #sigmasqfsamps = sigmasqflist[0]
+                    sigmasqfsamps = sigmasqflist[0]
 
             Om0samps = 8 * np.pi * rhozsamps[0, :] / (3 * Hzsamps[0, :] ** 2)
             OL0samps = Lamsamps / (3 * Hzsamps[0, :] ** 2)
@@ -133,7 +127,7 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF):
             dzdwdict[s] = dzdwzsamps
             Lamdict[s] = Lamsamps
             sigmasqidict[s] = sigmasqisamps
-            #sigmasqfdict[s] = sigmasqfsamps
+            sigmasqfdict[s] = sigmasqfsamps
             Om0dict[s] = Om0samps
             OL0dict[s] = OL0samps
 
@@ -142,7 +136,7 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF):
     # read in data
     zD, Dz, sDz = np.loadtxt(fname + 'Data/D.txt', unpack=True)
     zH, Hz, sHz = np.loadtxt(fname + 'Data/H.txt', unpack=True)
-    zrho, rhoz, srhoz = np.loadtxt(fname + 'Data/rho.txt', unpack=True)
+    #zrho, rhoz, srhoz = np.loadtxt(fname + 'Data/rho.txt', unpack=True)
     zdzdw, dzdwz, sdzdwz = np.loadtxt(fname + 'Data/dzdw.txt', unpack=True)
 
     # Create the figures we want to plot
@@ -234,18 +228,18 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF):
     axsigmasq[0].set_ylim(1e-13, 0.1)
 
 
-    # sigmasqfplh = plh(sigmasqfdict[files[0]], axsigmasq[1])
-    # sigmasqfplh.draw_Upper(l, sigmasqiF, sigmasqiLT)
-    # sigmasqfplh.add_plot(l, sigmasqfLT, col='m', lab=r'$LTB \ (t_B = 0)$')
-    # sigmasqfplh = plh(sigmasqfdict[files[1]], axsigmasq[1])
-    # sigmasqfplh.add_plot(l, sigmasqfplh.contours[:, 4], col='k', lab=labelsdict[files[1]])
-    # sigmasqfplh = plh(sigmasqfdict[files[2]], axsigmasq[1])
-    # sigmasqfplh.add_plot(l, sigmasqfplh.contours[:, 4], col='y', lab=labelsdict[files[2]])
-    #
-    # axsigmasq[1].set_ylabel(r'$  \log(\sigma^2_iD^2_i) $', fontsize=20)
-    # axsigmasq[1].set_xlabel(r'$ \frac{z}{z_{max}}$', fontsize=20)
-    # axsigmasq[1].set_yscale('log')
-    # axsigmasq[1].set_ylim(1e-13, 0.1)
+    sigmasqfplh = plh(sigmasqfdict[files[0]], axsigmasq[1])
+    sigmasqfplh.draw_Upper(l, sigmasqiF, sigmasqiLT)
+    sigmasqfplh.add_plot(l, sigmasqfLT, col='m', lab=r'$LTB \ (t_B = 0)$')
+    sigmasqfplh = plh(sigmasqfdict[files[1]], axsigmasq[1])
+    sigmasqfplh.add_plot(l, sigmasqfplh.contours[:, 4], col='k', lab=labelsdict[files[1]])
+    sigmasqfplh = plh(sigmasqfdict[files[2]], axsigmasq[1])
+    sigmasqfplh.add_plot(l, sigmasqfplh.contours[:, 4], col='y', lab=labelsdict[files[2]])
+
+    axsigmasq[1].set_ylabel(r'$  \log(\sigma^2_iD^2_i) $', fontsize=20)
+    axsigmasq[1].set_xlabel(r'$ \frac{z}{z_{max}}$', fontsize=20)
+    axsigmasq[1].set_yscale('log')
+    axsigmasq[1].set_ylim(1e-13, 0.1)
 
     handles, labels = axsigmasq[0].get_legend_handles_labels()
     p1 = Rectangle((0, 0), 1, 1, fc="red", alpha=0.5)
@@ -308,6 +302,7 @@ if __name__=="__main__":
     Np = GD["np"]
     Nret = GD["nret"]
     err = GD["err"]
+    samps_out_name = GD["samps_out_name"]
 
     # Do the plots
-    Plot_Data(zmax,Np,Nret,tstar,err,data_prior,data_lik,fname,Nsamp,DoPLCF)
+    Plot_Data(zmax,Np,Nret,tstar,err,data_prior,data_lik,fname,Nsamp,DoPLCF,samps_out_name)
