Fixed FLRW sigmasqF calculation

Master.py
@@ -25,7 +25,6 @@ from scipy.linalg import solve_triangular as soltri
 from scipy.interpolate import UnivariateSpline as uvs
 import matplotlib.pyplot as plt
 from Copernicus.fortran_mods import CIVP
-from genFLRW import FLRW
 import warnings
 warnings.simplefilter('error', UserWarning)
 warnings.simplefilter('error', RuntimeWarning)
@@ -586,38 +585,7 @@ class SSU(object):
             return 0.0
 
 
-    # def get_C_sol(self, Om0, Ok0, OL0, H0):
-    #     """
-    #     Since the Universe is FLRW along the central worldline (viz. C) we have analytic expressions for the input
-    #     functions along C. These can be used as boundary data in the CIVP (not currently implemented)
-    #     """
-    #     #First get current age of universe
-    #     amin = 0.5
-    #     t0 = quad(self.t0f,0,1.0,args=(Om0,Ok0,OL0,H0))[0]
-    #     #Get t(a) when a_0 = 1 (for some reason spline does not return correct result if we use a = np.linspace(1.0,0.2,1000)?)
-    #     a = np.linspace(amin,1.0,5000)
-    #     tmp = self.t0f(a,Om0,Ok0,OL0,H0)
-    #     dto = uvs(a,tmp,k=3,s=0)
-    #     to = dto.antiderivative()
-    #     t = t0 + to(a)
-    #     self.t = t
-    #     #Invert to get a(t)
-    #     aoto = uvs(t,a,k=3,s=0.0)
-    #     aow = aoto(self.w0)
-    #     #Now get rho(t)
-    #     rho0 = Om0*3*H0**2/(kappa)
-    #     rhoow = rho0*aow**(-3.0)
-    #     #Get How (this is up_0)
-    #     How = H0*np.sqrt(Om0*aow**(-3) + Ok0*aow**(-2) + OL0)
-    #     upow = How
-    #     #Now get dHz and hence uppow
-    #     #First dimensionless densities
-    #     Omow = kappa*rhoow/(3*How**2)
-    #     OLow = self.Lam/(3*How**2)
-    #     OKow = 1 - Omow - OLow
-    #     dHzow = How*(3*Omow + 2*OKow)/2 #(because the terms in the np.sqrt adds up to 1)
-    #     uppow = (dHzow + 2*upow)*upow
-    #     return rhoow, upow, uppow
+
         
     def affine_grid(self, Hz, rhoz, Lam):
         """
@@ -1083,142 +1051,142 @@ class SSU(object):
         #     Hperpstar = np.tile(self.Hperpstar[0],(self.Nret))
         return T1f, T2f, LLTBConsf, Df, Sf, Qf, Af, Zf, Spf, Qpf, Zpf, uf, upf, uppf, udotf, rhof, rhopf, rhodotf, sigmasqf
 
-if __name__ == "__main__":
-    #Set sparams zmax, tmin, Np, err, XH, Xrho, sigmaLam, Nret, data_prior, data_lik, fname
-    zmax = 2.5
-    tmin = 3.25
-    Np = 500
-    err = 1e-5
-    XH = np.array([0.6, 3.5])
-    Xrho = np.array([0.1, 1.5])
-    sigmaLam = 0.05
-    Nret = 300
-    data_prior = "[H,rho]"
-    data_lik = "[D,H,t0,dzdw]"
-    fname = "/home/landman/Projects/CP_LCDM_DHt0/"
-    DoPLCF = False
-
-    print "Getting LCDM vals"
-    # Get FLRW funcs for comparison
-    Om0 = 0.3
-    OL0 = 0.7
-    H0 = 0.2335
-    LCDM = FLRW(Om0, OL0, H0, zmax, Np)
-    HzF = LCDM.Hz
-    rhozF = LCDM.getrho()
-    Lam = 3 * OL0 * H0 ** 2
-    z = LCDM.z
-
-    print "Instantiating universe object"
-    U = SSU(zmax, tmin, Np, err, XH, Xrho, sigmaLam, Nret, data_prior, data_lik, fname, DoPLCF, beta=0.1, setLamPrior=True,
-            Hz=HzF, rhoz=rhozF, Lam=Lam)
-    logLik = U.logLik
-    Hz = U.Hz
-    rhoz = U.rhoz
-    Lam = U.Lam
-
-    plt.figure()
-    plt.plot(U.z, U.Hm, 'k')
-    plt.plot(U.z, U.Hz, 'b')
-    plt.errorbar(U.my_z_data['H'], U.my_F_data['H'], U.my_sF_data['H'], fmt='xr')
-    plt.show()
-
-
-    plt.figure()
-    plt.plot(U.z, U.rhom, 'k')
-    plt.plot(U.z, U.rhoz, 'b')
-    plt.errorbar(U.my_z_prior['rho'], U.my_F_prior['rho'], U.my_sF_prior['rho'], fmt='xr')
-    plt.show()
-
-    # # set grid of length scales
-    # ngrid = 500
-    # l = np.linspace(0.01,5,ngrid)
-    # logp = np.zeros(ngrid)
-    # XX = U.GPrho.XX
-    # y = U.GPrho.ydat
-    # n = U.GPrho.N
-    # sigmaf = U.Xrho[0]
-    # for i in xrange(ngrid):
-    #     theta = np.array([sigmaf,l[i]])
-    #     logp[i], _ = U.GPrho.logp_and_gradlogp(theta, XX, y, n)
+#if __name__ == "__main__":
+    # #Set sparams zmax, tmin, Np, err, XH, Xrho, sigmaLam, Nret, data_prior, data_lik, fname
+    # zmax = 2.5
+    # tmin = 3.25
+    # Np = 500
+    # err = 1e-5
+    # XH = np.array([0.6, 3.5])
+    # Xrho = np.array([0.1, 1.5])
+    # sigmaLam = 0.05
+    # Nret = 300
+    # data_prior = "[H,rho]"
+    # data_lik = "[D,H,t0,dzdw]"
+    # fname = "/home/landman/Projects/CP_LCDM_DHt0/"
+    # DoPLCF = False
     #
-    # lik = np.exp(-(logp - logp.min()))
-    # normfact = trapz(lik,l)
-    # lik /= normfact
-    # EDF = cumtrapz(lik,l,initial=0)
-    # I = np.argwhere(EDF < 0.16)
-    # print l[I[-1]]
-    # I = np.argwhere(EDF < 0.025)
-    # print l[I[-1]]
-    # plt.figure('rholik')
-    # plt.plot(l, EDF)
-    # plt.xlabel(r'$ l / [Gpc]$')
-    # plt.ylabel(r'$ CDF \ of \ GP \ \rho$ ')
+    # print "Getting LCDM vals"
+    # # Get FLRW funcs for comparison
+    # Om0 = 0.3
+    # OL0 = 0.7
+    # H0 = 0.2335
+    # LCDM = FLRW(Om0, OL0, H0, zmax, Np)
+    # HzF = LCDM.Hz
+    # rhozF = LCDM.getrho()
+    # Lam = 3 * OL0 * H0 ** 2
+    # z = LCDM.z
+    #
+    # print "Instantiating universe object"
+    # U = SSU(zmax, tmin, Np, err, XH, Xrho, sigmaLam, Nret, data_prior, data_lik, fname, DoPLCF, beta=0.1, setLamPrior=True,
+    #         Hz=HzF, rhoz=rhozF, Lam=Lam)
+    # logLik = U.logLik
+    # Hz = U.Hz
+    # rhoz = U.rhoz
+    # Lam = U.Lam
+    #
+    # plt.figure()
+    # plt.plot(U.z, U.Hm, 'k')
+    # plt.plot(U.z, U.Hz, 'b')
+    # plt.errorbar(U.my_z_data['H'], U.my_F_data['H'], U.my_sF_data['H'], fmt='xr')
     # plt.show()
-
-    accrate = np.zeros(2)
     #
-    # Do the burnin period
-    Nburn = 1000
-    Nsamps = 2000
-    Hsamps = np.zeros([Np,Nsamps])
-    rhosamps = np.zeros([Np, Nsamps])
-    Lamsamps = np.zeros([Nsamps])
-    # Hpsamps = np.zeros([Np,Nburn])
-    # rhopsamps = np.zeros([Np, Nburn])
-    # Lampsamps = np.zeros([Nburn])
-
-    # print "Sampling prior"
+    #
+    # plt.figure()
+    # plt.plot(U.z, U.rhom, 'k')
+    # plt.plot(U.z, U.rhoz, 'b')
+    # plt.errorbar(U.my_z_prior['rho'], U.my_F_prior['rho'], U.my_sF_prior['rho'], fmt='xr')
+    # plt.show()
+    #
+    # # # set grid of length scales
+    # # ngrid = 500
+    # # l = np.linspace(0.01,5,ngrid)
+    # # logp = np.zeros(ngrid)
+    # # XX = U.GPrho.XX
+    # # y = U.GPrho.ydat
+    # # n = U.GPrho.N
+    # # sigmaf = U.Xrho[0]
+    # # for i in xrange(ngrid):
+    # #     theta = np.array([sigmaf,l[i]])
+    # #     logp[i], _ = U.GPrho.logp_and_gradlogp(theta, XX, y, n)
+    # #
+    # # lik = np.exp(-(logp - logp.min()))
+    # # normfact = trapz(lik,l)
+    # # lik /= normfact
+    # # EDF = cumtrapz(lik,l,initial=0)
+    # # I = np.argwhere(EDF < 0.16)
+    # # print l[I[-1]]
+    # # I = np.argwhere(EDF < 0.025)
+    # # print l[I[-1]]
+    # # plt.figure('rholik')
+    # # plt.plot(l, EDF)
+    # # plt.xlabel(r'$ l / [Gpc]$')
+    # # plt.ylabel(r'$ CDF \ of \ GP \ \rho$ ')
+    # # plt.show()
+    #
+    # accrate = np.zeros(2)
+    # #
+    # # Do the burnin period
+    # Nburn = 1000
+    # Nsamps = 2000
+    # Hsamps = np.zeros([Np,Nsamps])
+    # rhosamps = np.zeros([Np, Nsamps])
+    # Lamsamps = np.zeros([Nsamps])
+    # # Hpsamps = np.zeros([Np,Nburn])
+    # # rhopsamps = np.zeros([Np, Nburn])
+    # # Lampsamps = np.zeros([Nburn])
+    #
+    # # print "Sampling prior"
+    # # for i in range(Nburn):
+    # #     Hz, rhoz, Lam, F = U.gen_sample(Hz, rhoz, Lam)
+    # #     Hpsamps[:,i] = Hz
+    # #     rhopsamps[:,i] = rhoz
+    # #     Lampsamps[i] = Lam
+    #
+    # print "Burning"
     # for i in range(Nburn):
-    #     Hz, rhoz, Lam, F = U.gen_sample(Hz, rhoz, Lam)
-    #     Hpsamps[:,i] = Hz
-    #     rhopsamps[:,i] = rhoz
-    #     Lampsamps[i] = Lam
-
-    print "Burning"
-    for i in range(Nburn):
-        Hz, rhoz, Lam, logLik, F, a = U.MCMCstep(logLik, Hz, rhoz, Lam)
-        U.track_max_lik(logLik, Hz, rhoz, Lam)
-
-    print "Resetting Lambda prior"
-    U.set_Lambda_Prior(U.Hz, U.rhoz)
-    print "sigmaLam = ", U.sigmaLam
-
-    print "Sampling"
-    for i in xrange(Nsamps):
-        Hz, rhoz, Lam, logLik, F, a = U.MCMCstep(logLik, Hz, rhoz, Lam)
-        Hsamps[:,i] = Hz
-        rhosamps[:,i] = rhoz
-        Lamsamps[i] = Lam
-        accrate += np.array([a, 1])
-
-    print "Accrate =", accrate[0]/accrate[1]
-
-    print "MaxLik Lam = ", U.Lamm, "with logLik = ", U.logLik
-
-    plt.figure()
-    plt.hist(Lamsamps,bins=25)
-    plt.show()
+    #     Hz, rhoz, Lam, logLik, F, a = U.MCMCstep(logLik, Hz, rhoz, Lam)
+    #     U.track_max_lik(logLik, Hz, rhoz, Lam)
     #
-    # plt.figure('Hp')
-    # plt.plot(z,Hpsamps,'b',alpha=0.2)
-    # plt.figure('rhop')
-    # plt.plot(z,rhopsamps,'b',alpha=0.2)
-    # plt.figure('Lamp')
-    # plt.hist(Lampsamps)
+    # print "Resetting Lambda prior"
+    # U.set_Lambda_Prior(U.Hz, U.rhoz)
+    # print "sigmaLam = ", U.sigmaLam
     #
-    plt.figure('H')
-    plt.plot(z, Hsamps, 'b', alpha=0.1)
-    plt.plot(U.z, U.Hm, 'k')
-    plt.plot(U.z, U.Hz, 'g')
-    plt.errorbar(U.my_z_data['H'], U.my_F_data['H'], U.my_sF_data['H'], fmt='xr')
-    plt.show()
-    plt.figure('rho')
-    plt.plot(z, rhosamps, 'b', alpha=0.1)
-    plt.plot(U.z, U.rhom, 'k')
-    plt.plot(U.z, U.rhoz, 'g')
-    plt.errorbar(U.my_z_prior['rho'], U.my_F_prior['rho'], U.my_sF_prior['rho'], fmt='xr')
-    plt.show()
+    # print "Sampling"
+    # for i in xrange(Nsamps):
+    #     Hz, rhoz, Lam, logLik, F, a = U.MCMCstep(logLik, Hz, rhoz, Lam)
+    #     Hsamps[:,i] = Hz
+    #     rhosamps[:,i] = rhoz
+    #     Lamsamps[i] = Lam
+    #     accrate += np.array([a, 1])
+    #
+    # print "Accrate =", accrate[0]/accrate[1]
+    #
+    # print "MaxLik Lam = ", U.Lamm, "with logLik = ", U.logLik
+    #
+    # plt.figure()
+    # plt.hist(Lamsamps,bins=25)
+    # plt.show()
+    # #
+    # # plt.figure('Hp')
+    # # plt.plot(z,Hpsamps,'b',alpha=0.2)
+    # # plt.figure('rhop')
+    # # plt.plot(z,rhopsamps,'b',alpha=0.2)
+    # # plt.figure('Lamp')
+    # # plt.hist(Lampsamps)
+    # #
+    # plt.figure('H')
+    # plt.plot(z, Hsamps, 'b', alpha=0.1)
+    # plt.plot(U.z, U.Hm, 'k')
+    # plt.plot(U.z, U.Hz, 'g')
+    # plt.errorbar(U.my_z_data['H'], U.my_F_data['H'], U.my_sF_data['H'], fmt='xr')
+    # plt.show()
+    # plt.figure('rho')
+    # plt.plot(z, rhosamps, 'b', alpha=0.1)
+    # plt.plot(U.z, U.rhom, 'k')
+    # plt.plot(U.z, U.rhoz, 'g')
+    # plt.errorbar(U.my_z_prior['rho'], U.my_F_prior['rho'], U.my_sF_prior['rho'], fmt='xr')
+    # plt.show()
     #
     # plt.show()
     # l = np.linspace(0,1,Nret)

genFLRW.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 """
 Created on Thu Aug 21 09:40:27 2014
 
@@ -10,7 +10,10 @@ Class to create FLRW universe object
 import numpy as np
 from numpy import linspace, sqrt, sin, sinh, pi, ones, zeros
 from scipy.interpolate import UnivariateSpline as uvs
+from scipy.integrate import quad
+from Copernicus import Master
 import matplotlib.pyplot as plt
+from Copernicus.Parset import MyOptParse
 
 class FLRW(object):
     def __init__(self,Om0,OL0,H0,zmax,NJ):
@@ -22,44 +25,70 @@ class FLRW(object):
         self.NJ = NJ
         self.z = linspace(0,zmax,self.NJ)
         self.delz = self.z[1]-self.z[0]
-        self.Hz = self.getHz(self.z)
+        self.Hz = self.getHz()
         self.Om = self.getOmz()
         self.OL = self.Lambda/(3*self.Hz**2)
         self.Ok = 1 - self.Om - self.OL        
-        self.getDz(self.z)
+        self.Dz = self.getDz()
         self.nuz = self.getnuz()
         self.nu = linspace(self.nuz[0],self.nuz[-1],self.NJ)
         self.rhoz = self.getrho()
-        
 
-    def getHz(self,z):
-        return self.H0*sqrt(self.Om0*(1+z)**3 + self.Ok0*(1+z)**2 + self.OL0)
+        # Set function to get t0
+        self.t0f = lambda x, a, b, c, d: np.sqrt(x) / (d * np.sqrt(a + b * x + c * x ** 3))
+
+    def getHz(self, z=None, Om0=None, OL0 = None, H0=None):
+        if Om0 is None and OL0 is None and H0 is None and z is None:
+            return self.H0*sqrt(self.Om0*(1+self.z)**3 + self.Ok0*(1+self.z)**2 + self.OL0)
+        else:
+            Ok0 = 1.0 - Om0 - OL0
+            return H0 * sqrt(Om0 * (1 + z) ** 3 + Ok0 * (1 + z) ** 2 + OL0)
             
     def getHo(self):
         Ho = uvs(self.z,self.Hz,k=5,s=0.0)
         return Ho
         
-    def getDz(self,z):
-        K = -self.Ok0*self.Hz[0]**2
-        dDc = uvs(self.z,1.0/self.Hz,k=3,s=0.0)
-        Dc = dDc.antiderivative()(self.z) #comoving distance
-        if (K>0.0):
-            self.Dz = sin(sqrt(K)*Dc)/sqrt(K)/(1+z)
-        elif (K<0.0):
-            self.Dz = sinh(sqrt(-K)*Dc)/sqrt(-K)/(1+z)
+    def getDz(self, z=None, Om0=None, OL0 = None, H0=None):
+        if Om0 is None and OL0 is None and H0 is None and z is None:
+            Om0 = self.Om0
+            OL0 = self.OL0
+            Ok0 = self.Ok0
+            H0 = self.H0
+            Hz = self.Hz
+            z = self.z
         else:
-            self.Dz = Dc/(1+self.z)
-        return
+            Ok0 = 1.0 - OL0 - Om0
+            Hz = self.getHz(z=z, Om0=Om0, OL0=OL0, H0=H0)
+        K = -Ok0*H0**2
+        dDc = uvs(z, 1.0/Hz, k=3, s=0.0)
+        Dc = dDc.antiderivative()(z) #comoving distance
+        if (K>0.0):
+            Dz = sin(sqrt(K)*Dc)/sqrt(K)/(1+z)
+        elif (K<0.0):
+            Dz = sinh(sqrt(-K)*Dc)/sqrt(-K)/(1+z)
+        else:
+            Dz = Dc/(1+z)
+        return Dz
+
         
     def getDo(self):
         Do = uvs(self.z,self.Dz,k=5,s=0.0)
         return Do
     
-    def getOmz(self):
-        return self.Om0*(1+self.z)**3*(self.H0/self.Hz)**2
+    def getOmz(self, z=None, Om0=None, H0=None, OL0=None):
+        if z is None and Om0 is None and H0 is None and OL0 is None:
+            return self.Om0*(1+self.z)**3*(self.H0/self.Hz)**2
+        else:
+            Hz = self.getHz(z, Om0=Om0, OL0=OL0, H0=H0)
+            return Om0 * (1 + z) ** 3 * (H0 / Hz) ** 2
         
-    def getrho(self):
-        return 3.0*self.Om*self.Hz**2.0/(8.0*pi)
+    def getrho(self, z=None, Om0=None, H0=None, OL0=None):
+        if z is None and Om0 is None and H0 is None and OL0 is None:
+            return 3.0*self.Om*self.Hz**2.0/(8.0*pi)
+        else:
+            Hz = self.getHz(z, Om0=Om0, OL0=OL0, H0=H0)
+            Om = self.getOmz(z, Om0=Om0, OL0=OL0, H0=H0)
+            return 3.0*Om*Hz**2.0/(8.0*pi)
         
     def getrhoo(self):
         rho = 3.0*self.Om*self.Hz**2.0/(8.0*pi)
@@ -74,30 +103,38 @@ class FLRW(object):
     def getdzdw(self):
         return self.H0*(1+self.z) - self.Hz
 
-    def get_sigmasq(self,DelRSq,UV_cut):
+    def get_sigmasq(self, DelRSq, UV_cut, ac, Hz, Dz, z):
         """
         Compute the magnitude of sigma^2*D^2/H^2 in perturbed FLRW
-        DelRSq = amplitude of primordial fluctuations
-        UV_cut = the UV cut-off
+        DelRSq  = amplitude of primordial fluctuations
+        UV_cut  = the UV cut-off
+        ac      = scale factor at central worldline
+        Hz      = the Hubble rate down PLC
+        Dz      = the angular diameter distance down PLC
+        z       = the redshift down PLC
         """
+        # rescale (1 + z) > (1 + z')/ac
+        uz_rescaled = (1 + z)/ac
+
         # Get normalisation of growth suppression function
         ginf = 2*(self.Om0**(4.0/7) - self.OL0 + (1+self.Om0/2.0)*(1+self.OL0/70.0))/(5*self.Om0)
 
         #Hubble scale
-        h = self.Hz[0]*299.8/100 #the 299.8 is a unit conversion
+        h = Hz[0]*299.8/100 #the 299.8 is a unit conversion
         kH0 = h/3.0e3
 
         # Normalised densities (remember to check against Julien's result
-        Omz = self.Om0*(1+self.z)**3/(self.OL0 + self.Om0*(1+self.z)**3)
-        OLz = self.OL0/(self.OL0 + self.Om0*(1+self.z)**3)
+        Omz = self.Om0*uz_rescaled**3/(self.OL0 + self.Om0*uz_rescaled**3)
+        OLz = self.OL0/(self.OL0 + self.Om0*uz_rescaled**3)
 
         # Growth suppression function and derived evolution function for the shear
-        g = 5*ginf*self.Om/(2*(self.Om**(4.0/7) - self.OL + (1+self.Om/2)*(1+self.OL/70)))
-        go = uvs(self.z, g, k=3, s=0.0)
+        #g = 5*ginf*self.Om/(2*(self.Om**(4.0/7) - self.OL + (1+self.Om/2)*(1+self.OL/70)))
+        g = 5 * ginf * Omz / (2 * (Omz ** (4.0 / 7) - OLz + (1 + Omz / 2) * (1 + OLz / 70)))
+        go = uvs(z, g, k=3, s=0.0)
         dgo = go.derivative()
-        dg = dgo(self.z)
+        dg = dgo(z)
 
-        G = (dg**2)**(1+self.z)**2 - 2*(1+self.z)*dg + g**2
+        G = (dg**2)**uz_rescaled**2 - 2*uz_rescaled*dg + g**2
 
         # Get the UV cut-off
         kUV = UV_cut/kH0
@@ -105,7 +142,8 @@ class FLRW(object):
         # Transfer function
         k = linspace(0,kUV,1000)
         q = k*kH0/(self.Om0*h**2)
-        T = np.log(1 + 2.34*q)/(2.34*q*(1+3.89*q + (16.1*q)**2 + (5.46*q)**3 + (6.71*q)**4))**0.25
+        T = np.zeros_like(k)
+        T[1::] = np.log(1 + 2.34*q[1::])/(2.34*q[1::]*(1+3.89*q[1::] + (16.1*q[1::])**2 + (5.46*q[1::])**3 + (6.71*q[1::])**4))**0.25
         T[0] = 0.0
 
         dSo = uvs(k, k**3*T**2, k=3, s=0.0)
@@ -113,23 +151,186 @@ class FLRW(object):
         S = So(kUV)
 
         # Expectation value of sigmasq*D**2/H**2
-        sigmasq = 4*DelRSq*G*S*self.Dz**2/((1+self.z)**2*75*self.Om0*ginf**2)
+        sigmasqDzsqoHzsq = 4*DelRSq*G*S*Dz**2/(uz_rescaled**2*75*self.Om0*ginf**2)
 
-        return sigmasq
+        return sigmasqDzsqoHzsq
 
+    def get_aot(self, Om0, OL0, H0):
+        """
+        Since the Universe is FLRW along the central worldline (viz. C) we have analytic expressions for the input
+        functions along C. These can be used as boundary data in the CIVP (not currently implemented)
+        """
+        Ok0 = 1.0 - Om0 - OL0
+        #First get current age of universe
+        t0 = quad(self.t0f, 0, 1.0, args=(Om0,Ok0,OL0,H0))[0]
+        #Get t(a) when a_0 = 1 (for some reason spline does not return correct result if we use a = np.linspace(1.0,0.2,1000)?)
+        a = np.linspace(0, 1.0, 5000)
+        tmp = self.t0f(a, Om0, Ok0, OL0, H0)
+        dto = uvs(a, tmp, k=3, s=0)
+        to = dto.antiderivative()
+        t = to(a)
+        #Invert to get a(t)
+        aoto = uvs(t, a, k=3, s=0.0)
+        return aoto, t0
+
+        # aow = aoto(self.w0)
+        # #Now get rho(t)
+        # rho0 = Om0*3*H0**2/(kappa)
+        # rhoow = rho0*aow**(-3.0)
+        # #Get How (this is up_0)
+        # How = H0*np.sqrt(Om0*aow**(-3) + Ok0*aow**(-2) + OL0)
+        # upow = How
+        # #Now get dHz and hence uppow
+        # #First dimensionless densities
+        # Omow = kappa*rhoow/(3*How**2)
+        # OLow = self.Lam/(3*How**2)
+        # OKow = 1 - Omow - OLow
+        # dHzow = How*(3*Omow + 2*OKow)/2 #(because the terms in the np.sqrt adds up to 1)
+        # uppow = (dHzow + 2*upow)*upow
+        # return rhoow, upow, uppow
+
+    def give_shear_for_plotting(self, Om0, OL0, H0, DelRSq, UV_cut, zmax, Np, tstar, Nret, data_prior, data_lik, fname,
+                                DoPLCF):
+        """
+        This gives the FLRW shear on the PLC0 and the PLCF
+        :param Om0:
+        :param OL0:
+        :param H0:
+        :param DelRSq:
+        :param UV_cut:
+        :return:
+        """
+        zi = np.linspace(0, zmax, Np)
+        Hzi = self.getHz(zi, Om0=Om0, OL0=OL0, H0=H0)
+        Dzi = self.getDz(zi, Om0=Om0, OL0=OL0, H0=H0)
+        sigmasqi = self.get_sigmasq(DelRSq, UV_cut, 1.0, Hzi, Dzi, zi)
+        sigmasqio = uvs(zi/zi[-1], sigmasqi, k=3, s=0.0)
+
+        LamF = 3 * OL0 * H0 ** 2
+        rhozi = self.getrho(zi, Om0=Om0, OL0=OL0, H0=H0)
+        Xrho = np.array([0.5, 2.8])
+        XH = np.array([0.6, 3.5])
+        sigmaLam = 0.6 * 3 * 0.7 * (70.0 / 299.79) ** 2
+        UF = Master.SSU(zmax, tstar, Np, err, XH, Xrho, sigmaLam, Nret, data_prior, data_lik, fname, DoPLCF, Hz=Hzi, rhoz=rhozi,
+                        Lam=LamF, useInputFuncs=True)
+
+        T1iF, T2iF, LLTBConsiF, DiF, SiF, QiF, AiF, ZiF, SpiF, QpiF, ZpiF, uiF, upiF, uppiF, udotiF, rhoiF, rhopiF, rhodotiF, \
+        DzF, dzdwzF, sigmasqiF, t0F = UF.get_funcsi()
+
+        if t0F > UF.tmin and UF.NI > 1 and DoPLCF:
+            T1fF, T2fF, LLTBConsfF, DfF, SfF, QfF, AfF, ZfF, SpfF, QpfF, ZpfF, ufF, upfF, uppfF, udotfF, rhofF, rhopfF, \
+            rhodotfF, sigmasqfF = UF.get_funcsf()
+
+        # Get background z, Dz, Hz for FLRW at t = tstar
+        zonuf = ufF - 1.0
+        zf = np.linspace(0, zonuf[-1], Nret)
+        DzFf = uvs(zonuf, DfF, k=3, s=0.0)(zf)
+        DzFf[0] = 0.0
+        HzFf = uvs(zonuf, upfF / ufF ** 2, k=3, s=0.0)(zf)
+
+        # Get aot
+        aot, t0 = LCDM.get_aot(Om0, OL0, H0)
+
+        ac = aot(tstar)
+
+        sigmasqf = LCDM.get_sigmasq(2.41e-9, 0.005, ac, HzFf, DzFf, zf)
+        sigmasqfo = uvs(zf / zf[-1], sigmasqf, k=3, s=0.0)
+
+        l = np.linspace(0, 1, Nret)
+
+        return Dzi, Hzi, rhozi, dzdwzF, sigmasqio(l), sigmasqfo(l)
 
 if __name__=="__main__":
-    OL = 0.7
-    Om = 0.3
+    # Get input args
+    GD = MyOptParse.readargs()
+
+    # Print out parset settings
+    keyslist = GD.keys()
+    for it in keyslist:
+        print it, GD[it]
+
+    #Determine how many samplers to spawn
+    NSamplers = GD["nwalkers"]
+    Nsamp = GD["nsamples"]
+    Nburn = GD["nburnin"]
+    tstar = GD["tstar"]
+    DoPLCF = GD["doplcf"]
+    DoTransform = GD["dotransform"]
+    fname = GD["fname"]
+    data_prior = GD["data_prior"]
+    data_lik = GD["data_lik"]
+    zmax = GD["zmax"]
+    Np = GD["np"]
+    Nret = GD["nret"]
+    err = GD["err"]
+    samps_out_name = GD["samps_out_name"]
+
+    OL0 = 0.7
+    Om0 = 0.3
     H0 = 70 / 299.8 #The 299.8 is a conversion factor to keep units consistent
-    zmax = 2.0
-    NJ = 500
-    
-    LCDM = FLRW(Om,OL,H0,zmax,NJ)
-    
-    #Units of rho are in Gpc^{-2} 
-    plt.plot(LCDM.z,LCDM.rhoz)
-    
-    
-    
-    
+    DelRSq = 2.41e-9
+    UV_cut = 0.005
+
+
+
+    LamF = 3 * OL0 * H0 ** 2
+
+    LCDM = FLRW(Om0, OL0, H0, zmax, Np)
+    # zi = LCDM.z
+    #
+    # Hzi = LCDM.Hz
+    # Dzi = LCDM.Dz
+    # rhozi = LCDM.rhoz
+
+    Dzi, Hzi, rhozi, dzdwzF, sigmasqi, sigmasqf = LCDM.give_shear_for_plotting(Om0, OL0, H0, DelRSq, UV_cut, zmax, Np, tstar, Nret, data_prior,
+                                                      data_lik, fname, DoPLCF)
+
+    # #print aot(t0), t0, aot(3.25)
+    #
+    # sigmasqi = LCDM.get_sigmasq(2.41e-9, 0.005, 1.0, Hzi, Dzi, zi)
+
+    # plot shear on PLC0
+    l = linspace(0, 1, Nret)
+    ymax = 1.1*sigmasqi.max()
+    plt.figure('PLC0')
+    plt.plot(l, sigmasqi, label='PLC0')
+    plt.ylim(1e-12, ymax)
+    #plt.yscale('log')
+    #plt.savefig(fname+'/Figures/sigmasqiF.png', dpi=250)
+
+    # # Do LCDM integration
+    # Xrho = np.array([0.5,2.8])
+    # XH = np.array([0.6,3.5])
+    # sigmaLam = 0.6 * 3 * 0.7 * (70.0 / 299.79) ** 2
+    # UF = Master.SSU(zmax, tstar, Np, err, XH, Xrho, sigmaLam, Nret, data_prior, data_lik, fname, DoPLCF, Hz=Hzi, rhoz=rhozi,
+    #          Lam=LamF, useInputFuncs=True)
+    #
+    # T1iF, T2iF, LLTBConsiF, DiF, SiF, QiF, AiF, ZiF, SpiF, QpiF, ZpiF, uiF, upiF, uppiF, udotiF, rhoiF, rhopiF, rhodotiF, \
+    # DzF, dzdwzF, sigmasqiF, t0F = UF.get_funcsi()
+    #
+    # if t0F > UF.tmin and UF.NI > 1 and DoPLCF:
+    #     T1fF, T2fF, LLTBConsfF, DfF, SfF, QfF, AfF, ZfF, SpfF, QpfF, ZpfF, ufF, upfF, uppfF, udotfF, rhofF, rhopfF, \
+    #     rhodotfF, sigmasqfF = UF.get_funcsf()
+    #
+    # # Get background z, Dz, Hz for FLRW at t = tstar
+    # zonuf = ufF - 1.0
+    # zf = np.linspace(0, zonuf[-1], Nret)
+    # DzFf = uvs(zonuf, DfF, k=3, s=0.0)(zf)
+    # DzFf[0] = 0.0
+    # HzFf = uvs(zonuf, upfF / ufF ** 2, k=3, s=0.0)(zf)
+    #
+    # # Get aot
+    # aot, t0 = LCDM.get_aot(Om0, OL0, H0)
+    #
+    # ac = aot(tstar)
+    #
+    # sigmasqf = LCDM.get_sigmasq(2.41e-9, 0.005, ac, HzFf, DzFf, zf)
+
+    #plt.figure('PLCF')
+    plt.plot(l, sigmasqf, label='PLCF')
+    plt.ylabel(r'$\log\left(\frac{\sigma^2 D^2}{H^2}\right)$')
+    plt.xlabel(r'$z$')
+    plt.legend()
+    #plt.ylim(1.0e-11, 1.0e-6)
+    plt.yscale('log')
+    plt.savefig(fname + '/Figures/logsigmasqfF.png', dpi=250)

master_plot.py
@@ -20,32 +20,19 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF,samps
     OL0 = 0.7
     H0 = 0.2335
     LCDM = FLRW(Om0, OL0, H0, zmax, Np)
-    HzF = LCDM.Hz
-    rhozF = LCDM.getrho()
-    sigmasqFz = LCDM.get_sigmasq(2.41e-9, 0.005) * HzF ** 2
-    z = LCDM.z
-    sigmasqo = uvs(z / z[-1], sigmasqFz, k=3, s=0.0)
+
+    DelRSq = 2.41e-9
+    UV_cut = 0.005
+    DzF, HzF, rhozF, dzdwzF, sigmasqiF, sigmasqfF = LCDM.give_shear_for_plotting(Om0, OL0, H0, DelRSq, UV_cut, zmax, Np,
+                                                                               tstar, Nret, data_prior,
+                                                                               data_lik, fname, DoPLCF)
 
     # Do integration of FLRW funcs
     zp = np.linspace(0, zmax, Np)
-    #zp2 = np.linspace(0, zmax, 200)
-    LamF = 3 * 0.7 * 0.2335 ** 2
-    Xrho = np.array([0.5,2.8])
-    XH = np.array([0.6,3.5])
+    Xrho = np.array([0.5, 2.8])
+    XH = np.array([0.6, 3.5])
     #set characteristic variance of Lambda prior (here 60%)
     sigmaLam = 0.6*3*0.7*(70.0/299.79)**2
-
-    # Do LCDM integration
-    UF = SSU(zmax, tmin, Np, err, XH, Xrho, sigmaLam, Nret, data_prior, data_lik, fname, DoPLCF, Hz=HzF, rhoz=rhozF, Lam=LamF, useInputFuncs=True)
-
-    T1iF, T2iF, LLTBConsiF, DiF, SiF, QiF, AiF, ZiF, SpiF, QpiF, ZpiF, uiF, upiF, uppiF, udotiF, rhoiF, rhopiF, rhodotiF, \
-    DzF, dzdwzF, sigmasqiF, t0F = UF.get_funcsi()
-
-    if t0F > UF.tmin and UF.NI > 1 and DoPLCF:
-        T1fF, T2fF, LLTBConsfF, DfF, SfF, QfF, AfF, ZfF, SpfF, QpfF, ZpfF, ufF, upfF, uppfF, udotfF, rhofF, rhopfF, \
-        rhodotfF, sigmasqfF = UF.get_funcsf()
-
-    sigmasqiF = sigmasqo(np.linspace(0, 1, Nret))
 
     # Do LTB integration
     print "Getting LTB vals"
@@ -229,7 +216,7 @@ def Plot_Data(zmax,Np,Nret,tmin,err,data_prior,data_lik,fname,Nsamp,DoPLCF,samps
 
 
     sigmasqfplh = plh(sigmasqfdict[files[0]], axsigmasq[1])
-    sigmasqfplh.draw_Upper(l, sigmasqiF, sigmasqiLT)
+    sigmasqfplh.draw_Upper(l, sigmasqfF, sigmasqiLT)
     sigmasqfplh.add_plot(l, sigmasqfLT, col='m', lab=r'$LTB \ (t_B = 0)$')
     sigmasqfplh = plh(sigmasqfdict[files[1]], axsigmasq[1])
     sigmasqfplh.add_plot(l, sigmasqfplh.contours[:, 4], col='k', lab=labelsdict[files[1]])